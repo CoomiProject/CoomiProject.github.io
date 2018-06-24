069870439dad31dcb3584479d7376dcc9b1938ef
Erichin <erichin@r.recruit.co.jp>
2018-06-24 22:26:14 +0900

    add pug

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const fs = require('fs');
+const pug = require('pug');
+const yaml = require('js-yaml');
+const fn = pug.compileFile('index.pug', {pretty: true});
+
+fs.writeFileSync('zh/index.html', fn(yaml.load(fs.readFileSync('locals/zh.yml', 'utf8'))));
+// fs.writeFileSync('en/index.html', fn(yaml.load(fs.readFileSync('locals/en.yml', 'utf8'))));
+// fs.writeFileSync('ja/index.html', fn(yaml.load(fs.readFileSync('locals/ja.yml', 'utf8'))));
