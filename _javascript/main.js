a2eb5e8fd73e03c297e2a2565b6ade4586077697
Erichin <erichin@r.recruit.co.jp>
2018-07-01 14:34:50 +0900

    update

--- _javascript/main.js
+++ _javascript/main.js
@@ -29,6 +29,12 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   }
 
+  var dropdown = document.querySelector('.dropdown');
+  dropdown.addEventListener('click', function(event) {
+    event.stopPropagation();
+    dropdown.classList.toggle('is-active');
+  });
+
 });
 
 $(document).ready(function(){
